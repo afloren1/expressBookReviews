3d4e9b0683201d80a5a9f817169de824a420c120
afloren1 <bluemoon66063@gmail.com>
2024-06-24 22:27:07 -0400

    added code to get list of books,added code for authentication mechanism, and added code for isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,18 +13,24 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
+    res.send(JSON.stringify(books,null,3));
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
+  const isbn = req.params.isbn;
+    res.send(books[isbn]);
   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
+  const author = req.params.author;
+    let filtered_books = books.filter((books) => books.author === author);
+    res.send(filtered_books);
   return res.status(300).json({message: "Yet to be implemented"});
 });
 

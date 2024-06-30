a27dfe0eaead4b93af3ceaf798cc4efbbacf5a84
afloren1 <bluemoon66063@gmail.com>
2024-06-30 19:27:03 -0400

    added registration and login

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,6 +7,18 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    if (!isValid(username)) {
+      users.push({ "username": username, "password": password });
+      return res.status(200).json({ message: "User " + req.body.username + " has been added." });
+    } else {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+  }
+  return res.status(404).json({ message: "Unable to register user." });
+   
   return res.status(300).json({message: "Yet to be implemented"});
 });
 

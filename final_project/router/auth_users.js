06f5f62585263a9f59b29ea9281f19b297e6fd38
afloren1 <bluemoon66063@gmail.com>
2024-07-05 16:01:23 -0400

    added route to delete a review in auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -92,6 +92,30 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   }
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn; // Get the ISBN from the URL parameter
+    const review = req.query.review;
+    const username = req.session.username; // Get the username from the session
+      
+    let foundBook = null;
+    for (let key in books) {
+      if (books[key].isbn === isbn) {
+        foundBook = books[key];
+      }
+    }
+    if (foundBook) {
+      // Initialize reviews if not already present
+      if (foundBook.reviews && foundBook.reviews[username]) {
+        // Delete the user's review
+        delete foundBook.reviews[username];
+        res.send(`User ${username} deleted their review.`);
+    } else {
+        res.status(404).send("Review not found for this user.");
+    }
+} else {
+    res.status(404).send("Unable to find book!");
+}
+});
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
